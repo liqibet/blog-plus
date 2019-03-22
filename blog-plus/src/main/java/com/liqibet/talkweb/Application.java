0212b5a5c444af058ea2409f1cfa066bd1086401
liqibet <liqibet@foxmail.com>
2019-03-22 16:53:57 +0800

    添加main方法

---
+++ blog-plus/src/main/java/com/liqibet/talkweb/Application.java
@@ -0,0 +1,13 @@
+package com.liqibet.talkweb;
+
+public class Application {
+
+    public static void main(String[] args){
+
+        System.out.println("================Show me the code!==================");
+
+
+
+    }
+
+}
